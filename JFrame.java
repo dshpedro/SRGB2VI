60f3a8b7d45b53e13d4dd73704400ed01d5af417
Pedro <pedrohpisantos@gmail.com>
2023-03-14 16:46:58 -0300

    Create JFrame.java

---
+++ JFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class main {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.setTitle("SRGB2VI");
+		f.setSize(800, 600);
+		f.getContentPane().setBackground(Color.DARK_GRAY);
+		f.setLocationRelativeTo(null);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+
+}
