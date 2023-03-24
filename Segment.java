ecf3659d44deed871dd566f234ef411fbde576ce
Pedro <pedrohpisantos@gmail.com>
2023-03-24 18:26:43 -0300

    Segment class working

--- Segment.java
+++ Segment.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
@@ -10,28 +11,135 @@ import javax.swing.SwingConstants;
 
 public class Segment extends JPanel {
 
-    ImageIcon segmentImage = new ImageIcon();
-    JLabel imageLabel = new JLabel();
-    JTextField tbxSegmentInitialRGBI = new JTextField();
-    
+    JLabel lbImageLabel = new JLabel();
+    JLabel lbR = new JLabel();
+    JLabel lbG = new JLabel();
+    JLabel lbB = new JLabel();
+    JTextField tbxInitialRedI = new JTextField();
+    JTextField tbxInitialGreenI = new JTextField();   
+    JTextField tbxInitialBlueI = new JTextField();
+    JTextField tbxEndingRedI = new JTextField();
+    JTextField tbxEndingGreenI = new JTextField();   
+    JTextField tbxEndingBlueI = new JTextField();   
 
     Segment(){
         this.setBackground(Color.DARK_GRAY);
-        this.setPreferredSize(new Dimension(800, 100));
+        this.setPreferredSize(new Dimension(99, 155));
+        this.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 5));
 
-        this.tbxSegmentInitialRGBI.setPreferredSize(new Dimension(40, 20));
-        this.tbxSegmentInitialRGBI.setHorizontalAlignment(SwingConstants.CENTER);
-        this.add(tbxSegmentInitialRGBI);
-        this.add(imageLabel);
+        lbR.setText("R");
+        lbR.setPreferredSize(new Dimension(30, 20));;
+        lbR.setHorizontalAlignment(SwingConstants.CENTER);
+        lbR.setForeground(Color.RED);
+        this.add(lbR);
+
+        lbG.setText("G");
+        lbG.setPreferredSize(new Dimension(30, 20));;
+        lbG.setHorizontalAlignment(SwingConstants.CENTER);
+        lbG.setForeground(Color.GREEN);
+        this.add(lbG);
+
+        lbB.setText("B");
+        lbB.setPreferredSize(new Dimension(30, 20));;
+        lbB.setHorizontalAlignment(SwingConstants.CENTER);
+        lbB.setForeground(Color.BLUE);
+        this.add(lbB);  
+
+        this.tbxInitialRedI.setPreferredSize(new Dimension(30, 20));
+        this.tbxInitialRedI.setHorizontalAlignment(SwingConstants.CENTER);
+        this.tbxInitialRedI.addActionListener(e -> System.out.println("red pressed"));
+        this.tbxInitialRedI.addActionListener(e -> this.updateSegment());
+        this.add(tbxInitialRedI);
+
+        this.tbxInitialGreenI.setPreferredSize(new Dimension(30, 20));
+        this.tbxInitialGreenI.setHorizontalAlignment(SwingConstants.CENTER);
+        this.tbxInitialGreenI.addActionListener(e -> System.out.println("green pressed"));
+        this.tbxInitialGreenI.addActionListener(e -> this.updateSegment());
+        this.add(tbxInitialGreenI);
+
+        this.tbxInitialBlueI.setPreferredSize(new Dimension(30, 20));
+        this.tbxInitialBlueI.setHorizontalAlignment(SwingConstants.CENTER);
+        this.tbxInitialBlueI.addActionListener(e -> System.out.println("blue pressed"));
+        this.tbxInitialBlueI.addActionListener(e -> this.updateSegment());
+        this.add(tbxInitialBlueI);
+
+        this.tbxEndingRedI.setPreferredSize(new Dimension(30, 20));
+        this.tbxEndingRedI.setHorizontalAlignment(SwingConstants.CENTER);
+        this.tbxEndingRedI.addActionListener(e -> System.out.println("red pressed"));
+        this.tbxEndingRedI.addActionListener(e -> this.updateSegment());
+        this.add(tbxEndingRedI);
+
+        this.tbxEndingGreenI.setPreferredSize(new Dimension(30, 20));
+        this.tbxEndingGreenI.setHorizontalAlignment(SwingConstants.CENTER);
+        this.tbxEndingGreenI.addActionListener(e -> System.out.println("green pressed"));
+        this.tbxEndingGreenI.addActionListener(e -> this.updateSegment());
+
+        this.add(tbxEndingGreenI);
+
+        this.tbxEndingBlueI.setPreferredSize(new Dimension(30, 20));
+        this.tbxEndingBlueI.setHorizontalAlignment(SwingConstants.CENTER);
+        this.tbxEndingBlueI.addActionListener(e -> System.out.println("blue pressed"));
+        this.tbxEndingBlueI.addActionListener(e -> this.updateSegment());
+        this.add(tbxEndingBlueI);
+
+        this.add(lbImageLabel);
     }
 
-    void updateSegment(BufferedImage image){
+    void updateSegment(){
+        BufferedImage image = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB); 
+
+        int[] initialRGBI = {0, 0, 0};
+		int[] endingRGBI = {0, 0, 0};
+        try {
+            initialRGBI[0] = Integer.parseInt(tbxInitialRedI.getText());
+            initialRGBI[1] = Integer.parseInt(tbxInitialGreenI.getText());
+            initialRGBI[2] = Integer.parseInt(tbxInitialBlueI.getText());
+            endingRGBI[0] = Integer.parseInt(tbxEndingRedI.getText());
+            endingRGBI[1] = Integer.parseInt(tbxEndingGreenI.getText());
+            endingRGBI[2] = Integer.parseInt(tbxEndingBlueI.getText());
+        } catch (Exception e) {
+            return;
+        }
         
-        this.segmentImage.setImage(image);
-		this.imageLabel.setIcon(segmentImage);
-        
+
+        int red, green, blue, redI=initialRGBI[0], greenI=initialRGBI[1], blueI=initialRGBI[2];
+        for (int x = 0; x < 100; x++) {
+			for (int y = 0; y < 100; y++) {
+				red = Math.round( redI * 255 / 100 );
+				green = Math.round( greenI * 255 / 100 );
+				blue = Math.round( blueI * 255 / 100 );
+
+				int rgb = red;
+				rgb = (rgb << 8) + green; 
+				rgb = (rgb << 8) + blue;
+
+				image.setRGB(x, y, rgb);
+			}
+
+            if (redI < endingRGBI[0]) {
+				redI++;
+			}
+			else if (redI > endingRGBI[0]) {
+				redI--;
+			}
+			
+			if (greenI < endingRGBI[1]) {
+				greenI++;
+			}
+			else if (greenI > endingRGBI[1]) {
+				greenI--;
+
+			}if (blueI < endingRGBI[2]) {
+				blueI++;
+			}
+			else if (blueI > endingRGBI[2]) {
+				blueI--;
+			}
+        }
+
+        ImageIcon segmentImage = new ImageIcon();
+        segmentImage.setImage(image);
+		lbImageLabel.setIcon(segmentImage);
         System.out.println("the button was pressed");
     }
-    
-    
 }
--- main.java
+++ main.java
@@ -1,9 +1,9 @@
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.image.BufferedImage;
-
-import javax.imageio.ImageIO;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import java.io.*;
 
@@ -13,82 +13,10 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		JComboBox<Integer> cmbNumberOfSegments = new JComboBox<>(new Integer[]{1, 2});
+		JComboBox<Integer> cmbNumberOfSegments = new JComboBox<>(new Integer[]{1, 2, 3, 4, 5});
 		cmbNumberOfSegments.setBounds(300, 50, 50, 25);
 		cmbNumberOfSegments.setSelectedIndex(1);
-
 		int numberOfSegments = Integer.parseInt(cmbNumberOfSegments.getSelectedItem().toString());
-		int segmentSize = 100; 
-		int imgWidth = numberOfSegments * segmentSize;
-		int imgHeight = numberOfSegments * segmentSize;
-		BufferedImage image = new BufferedImage(imgWidth, imgHeight, BufferedImage.TYPE_INT_RGB); 
-		
-		int redI=0, greenI=0, blueI=0;
-		int red, green, blue;
-		int currentSegment = 1;
-		int[] EndingRGBI = {0, 0, 0};
-		int[] InitialRGBI = getCurrentSegmentInitialRGBI(currentSegment);
-		redI = InitialRGBI[0];
-		greenI = InitialRGBI[1];
-		blueI = InitialRGBI[2];
-		EndingRGBI = getCurrentSegmentEndingRGBI(currentSegment);
-
-		for (int x = 0; x < imgWidth; x++) {
-			
-			if (x == segmentSize * currentSegment) {
-				currentSegment++;
-				InitialRGBI = getCurrentSegmentInitialRGBI(currentSegment);
-				redI = InitialRGBI[0];
-				greenI = InitialRGBI[1];
-				blueI = InitialRGBI[2];
-				EndingRGBI = getCurrentSegmentEndingRGBI(currentSegment);
-				
-			}
-			
-
-			for (int y = 0; y < imgHeight; y++) {
-				red = Math.round( redI * 255 / 100 );
-				green = Math.round( greenI * 255 / 100 );
-				blue = Math.round( blueI * 255 / 100 );
-
-				int rgb = red;
-				rgb = (rgb << 8) + green; 
-				rgb = (rgb << 8) + blue;
-
-				image.setRGB(x, y, rgb);
-			}
-			
-			if (redI < EndingRGBI[0]) {
-				redI++;
-			}
-			else if (redI > EndingRGBI[0]) {
-				redI--;
-			}
-			
-			if (greenI < EndingRGBI[1]) {
-				greenI++;
-			}
-			else if (greenI > EndingRGBI[1]) {
-				greenI--;
-
-			}if (blueI < EndingRGBI[2]) {
-				blueI++;
-			}
-			else if (blueI > EndingRGBI[2]) {
-				blueI--;
-			}
-
-		}
-        
-		System.out.println("Done");
-
-		File outputFile = new File("output.bmp");
-		outputFile.createNewFile();
-        ImageIO.write(image, "bmp", outputFile);
-
-
-		Segment segment1 = new Segment();
-
 
 		JLabel lbNumberOfSegments = new JLabel();
 		lbNumberOfSegments.setText("Number of segments");
@@ -96,19 +24,19 @@ public class Main {
 		lbNumberOfSegments.setHorizontalAlignment(JLabel.CENTER);
 		lbNumberOfSegments.setVerticalAlignment(JLabel.TOP);
 		lbNumberOfSegments.setBounds(300, 0, 200, 200);
-
 		
-		JButton btnPrintSegment = new JButton();
-		btnPrintSegment.setText("Print Segment(s)");
-        btnPrintSegment.addActionListener(e -> segment1.updateSegment(image));
+		JButton btnAddSegment = new JButton();
+		btnAddSegment.setText("Add segment");
+		JButton btnReset = new JButton();
+		btnReset.setText("Reset");
 
 		JPanel pnlControlPanel = new JPanel();
 		pnlControlPanel.setBackground(Color.DARK_GRAY);
 		pnlControlPanel.setPreferredSize(new Dimension(800, 100));
 		pnlControlPanel.add(lbNumberOfSegments);
 		pnlControlPanel.add(cmbNumberOfSegments);
-		pnlControlPanel.add(btnPrintSegment);
-
+		pnlControlPanel.add(btnAddSegment);
+		pnlControlPanel.add(btnReset);
 
 		JFrame frame = new JFrame();
 		frame.setTitle("SRGB2VI");
@@ -116,59 +44,38 @@ public class Main {
 		frame.getContentPane().setBackground(Color.DARK_GRAY);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new FlowLayout());
+		frame.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
+		frame.setResizable(false);
 		frame.add(pnlControlPanel);
-		frame.add(segment1);
-
+		btnAddSegment.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e){
+				frame.add(new Segment());
+				frame.revalidate();
+				frame.repaint();
+			}
+		});
+		for (int i = 0; i < numberOfSegments; i++) {
+			frame.add(new Segment());
+		}
 		frame.setVisible(true);
-		
+
+		cmbNumberOfSegments.addActionListener(e -> frame.add(new Segment()));
+
+		btnReset.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e){
+				Component[] componentList = frame.getRootPane().getContentPane().getComponents();
+				for(Component c : componentList){
+					if(c instanceof Segment){
+						frame.remove(c);
+					}
+				}
+				frame.revalidate();
+				frame.repaint();
+			}
+		});
+
 	}
 
-	static int[] getCurrentSegmentInitialRGBI(int currentSegment) {
-		int[] rgbIArray = {0, 0, 0};
-
-		switch (currentSegment) {
-			case 1:
-				rgbIArray[0] = 100;
-				rgbIArray[1] = 0;
-				rgbIArray[2] = 100;
-				break;
-			case 2:
-				rgbIArray[0] = 100;
-				rgbIArray[1] = 100;
-				rgbIArray[2] = 0;
-				break;
-			default:
-				rgbIArray[0] = 0;
-				rgbIArray[1] = 0;
-				rgbIArray[2] = 0;
-				break;
-		}
-
-		return rgbIArray;
-	}
-
-	static int[] getCurrentSegmentEndingRGBI(int currentSegment) {
-		int[] rgbIArray = {0, 0, 0};
-
-		switch (currentSegment) {
-			case 1:
-				rgbIArray[0] = 0;
-				rgbIArray[1] = 100;
-				rgbIArray[2] = 0;
-				break;
-			case 2:
-				rgbIArray[0] = 0;
-				rgbIArray[1] = 0;
-				rgbIArray[2] = 100;
-				break;
-			default:
-				rgbIArray[0] = 0;
-				rgbIArray[1] = 0;
-				rgbIArray[2] = 0;
-				break;
-		}
-
-		return rgbIArray;
-	}	
 }
