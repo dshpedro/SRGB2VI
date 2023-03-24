c502edf22e757e8797f67467f9842a9f7867820e
Pedro <pedrohpisantos@gmail.com>
2023-03-24 13:55:48 -0300

    develop branch created

--- Segment.java
+++ Segment.java
@@ -5,16 +5,23 @@ import java.awt.image.BufferedImage;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class Segment extends JPanel {
 
     ImageIcon segmentImage = new ImageIcon();
     JLabel imageLabel = new JLabel();
+    JTextField tbxSegmentInitialRGBI = new JTextField();
     
 
     Segment(){
         this.setBackground(Color.DARK_GRAY);
         this.setPreferredSize(new Dimension(800, 100));
+
+        this.tbxSegmentInitialRGBI.setPreferredSize(new Dimension(40, 20));
+        this.tbxSegmentInitialRGBI.setHorizontalAlignment(SwingConstants.CENTER);
+        this.add(tbxSegmentInitialRGBI);
         this.add(imageLabel);
     }
 
